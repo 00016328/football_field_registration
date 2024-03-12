d8f35b85a37b637bf79de264c5fae4f9f3083f70
00016328 <00016328wiut@gmail.com>
2024-03-12 23:57:09 +0500

    update/add template is added and styled

--- controllers/clients/index.js
+++ controllers/clients/index.js
@@ -1,7 +1,13 @@
 const clients_controller = {
     index: async (req, res) =>{
-        res.render('clients');
-    }  
+        res.render('layout');
+    },
+    add: async (req, res) =>{
+        res.render('layout/update_add');
+    },
+    update: async (req, res) =>{
+        res.render('layout/update_add');
+    }
 };
   
 module.exports = clients_controller;
--- routes/clients/index.js
+++ routes/clients/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const clients_controller = require('../../controllers/clients');
 
 router.get('/', clients_controller.index);
+router.get('/add', clients_controller.add);
+router.get('/update', clients_controller.update);
 
 module.exports = router;
